725ea8ff03c5878f59439c10c720dd7ea4828267
Telchark <andrzej.boba361@gmail.com>
2020-03-01 15:29:08 +0100

    lab

---
+++ Lab1/program/program/DB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+namespace ADO.NET
+{
+    class DB
+    {
+        public void Select(SqlConnection connection)
+        {
+            var sql = "SELECT * FROM Region";
+            var command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(String.Format($"{reader[0]},{reader[1]}"));
+            }
+        }
+        public void Insert(SqlConnection connection, string regionName, int id)
+        {
+
+            var sql = "INSERT INTO Region(RegionID,RegionDescription) VALUES (@id,@regionName)";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@regionName", regionName);
+
+            int affected = command.ExecuteNonQuery();
+
+            Console.WriteLine($"{affected} rows");
+        }
+        public void Delete(SqlConnection connection, int id)
+        {
+            var sql = "DELETE FROM Region WHERE RegionID=@id";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            int affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows");
+        }
+        public void Update(SqlConnection connection, int id, string newRegion)
+        {
+            var sql = "UPDATE Region SET RegionDescription=@newRegion WHERE RegionId=@id";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@newReg", newRegion);
+            int affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows");
+        }
+
+
+    }
+}
---
+++ Lab1/program/program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            var db = new DB();
+            db.Select(connection);
+            //db.Insert(connection, "Bielsko", 5);
+            //db.Delete(connection, 11);
+            //db.Update(connection, 5, "Bielsko-Biała");
+            connection.Close(); //optional
+        }
+    }
+}
