c4cc9ee9515fe9d454e0370308abd6b94b0cdd77
Telchark <andrzej.boba361@gmail.com>
2020-03-25 09:29:01 +0100

    Lab4

--- Lab4/lab4/TeamsContext.cs
+++ Lab4/lab4/TeamsContext.cs
@@ -8,6 +8,7 @@ namespace lab4
     public class TeamsContext : DbContext
     {
         public DbSet<Team> Teams { get; set; }
+		public DbSet<Coach> Coaches { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Programowanie;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
