186eeb7d9318ff5a6c417cd70eb331a6cc105406
Telchark <andrzej.boba361@gmail.com>
2020-05-08 01:01:00 +0200

    Lab

---
+++ Lab10/WpfApp/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FileContent fileContent = new FileContent();
+        private FileSystemWatcher fileWatcher = new FileSystemWatcher();
+        public MainWindow()
+        {
+            InitializeComponent();
+            TB.DataContext = fileContent;
+            fileWatcher.NotifyFilter = NotifyFilters.LastWrite;
+            fileWatcher.Changed += watcherReaction;
+        }
+        /// <summary>
+        /// Paczacz paczy i co sie dzieje jak zobaczy
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void watcherReaction(object sender, FileSystemEventArgs e)
+        {
+            var txt = File.ReadAllText(fileContent.Path);
+            fileContent.Content = txt;
+        }
+        /// <summary>
+        /// Wczytanie pliku
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnChooseFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog()
+            {
+                Title = "Wybierz plik",
+                Filter="Textfile|*.txt"
+            };
+            var getFile = fileDialog.ShowDialog(this);
+            if(getFile.HasValue || getFile.Value)
+            {
+                fileContent.Path = fileDialog.FileName;
+                fileWatcher.Path = fileDialog.FileName.Replace(fileDialog.SafeFileName, string.Empty);
+                fileWatcher.EnableRaisingEvents = true;
+                var txt = File.ReadAllText(fileContent.Path);
+                fileContent.Content = txt;
+            }
+        }
+        /// <summary>
+        /// Zawartość pliku z kontrolą czy nie zapisujemy tego samego
+        /// </summary>
+        public class FileContent : INotifyPropertyChanged
+        {
+            public string Path { get; set; }
+            private string _content;
+            public string Content
+            {
+                get { return _content; }
+                set
+                {
+                    if (_content != value)
+                    {
+                        _content = value;
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Content"));
+                    }
+                }
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+    }
+}
---
+++ Lab9/Lab9/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab9
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var view = new StudentView();
+            this.DataContext = view;
+            LB1.ItemsSource = view.List.students;
+        }
+
+    }
+    /// <summary>
+    /// Studenciak
+    /// </summary>
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string FullName => Name +" " + LastName;
+        public DateTime Enlisting { get; set; }
+        public override string ToString()
+        {
+            return FullName;
+        }
+    }
+    public class StudentList
+    {
+        public ObservableCollection<Student> students { get; set; }
+    }
+
+    public class StudentView
+    {
+        public StudentList List { get; set; }
+        public StudentView()
+        {
+            List = new StudentList()
+            {
+                students = new ObservableCollection<Student>()
+            {
+                new Student
+                {
+                    Id = 1,
+                    Name = "Andrzej",
+                    LastName = "Boba",
+                    Enlisting = new DateTime(1998, 1, 1)
+                },
+                new Student
+                {
+                    Id = 2,
+                    Name = "Jan",
+                    LastName = "Kowalski",
+                    Enlisting = new DateTime(2002, 1, 1)
+                },
+                new Student
+                {
+                    Id = 3,
+                    Name = "Genowefa",
+                    LastName = "Konstantynopolitańczykiewiczówna",
+                    Enlisting = new DateTime(1970, 1, 1)
+                }
+            }
+            };
+    }
+}
+
+}
