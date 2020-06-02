8ba9f68872b89fde73bfa80326277c0b2ba32988
Telchark <andrzej.boba361@gmail.com>
2020-06-02 20:05:44 +0200

    Lab13

---
+++ Lab13/WpfApp/Context.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace WpfApp
+{
+    public class Context : DbContext
+    {
+        public DbSet<Model> Models { get; set; }
+        public Context()
+        {
+
+            Models.Add(new Model() { Amount = 5, Text = "test" });
+
+        }
+    }
+}
